3360a535dd84e5063b2e4b1d3704a772b52a0b57
Louis Heery <zcaplhe@ucl.ac.uk>
2019-10-13 21:36:53 +0100

    Update boosted-decision-tree.py

--- boosted-decision-tree.py
+++ boosted-decision-tree.py
@@ -4,15 +4,10 @@
 import pandas
 import numpy
 import sys
-sys.path.append("../")
-sys.path.append("../dataset-and-plotting")
 import pickle
 
 import matplotlib.cm as cm
 from sklearn.preprocessing import scale
-
-from ../bdtPlotting import *
-from ../sensitivity import *
 from xgboost import XGBClassifier
 from IPython.display import display
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
@@ -20,70 +15,46 @@ from sklearn.tree import DecisionTreeClassifier
 import time
 import threading
 
-def totalSensitivity(A,B,errorA,errorB):
-    totalSensitivity = np.sqrt(A**2 + B**2)
-    totalError = np.sqrt(((A*errorA)/np.sqrt(A**2 + B**2))**2 + ((B*errorB)/np.sqrt(A**2 + B**2))**2)
-
-    return (totalSensitivity,totalError)
-
 start = time.time()
-
-for nJets in [2,3]:
 
 print("************")
 print("STARTED XGBoost Classifier")
 
-if nJets == 2:
+# Model Input Variables
+variables = ['variable1', 'variable2', 'variable3']  # ENTER INPUT VARIABLES HERE = Column Names of CSV File
 
-    variables = ['mBB', 'dRBB', 'pTB1', 'pTB2', 'MET', 'dPhiVBB', 'dPhiLBmin', 'Mtop', 'dYWH', 'mTW', 'pTV', 'MV1cB1_cont', 'MV1cB2_cont', 'nTrackJetsOR',]
-
-    n_estimators = 200
-    max_depth = 4
-    learning_rate = 0.15
-    subsample = 0.5
-
-else:
-
-    variables = ['mBB', 'dRBB', 'pTB1', 'pTB2', 'MET', 'dPhiVBB', 'dPhiLBmin', 'Mtop', 'dYWH', 'mTW', 'pTV', 'mBBJ', 'pTJ3', 'MV1cB1_cont', 'MV1cB2_cont', 'MV1cJ3_cont','nTrackJetsOR',]
-
-    n_estimators = 200
-    max_depth = 4
-    learning_rate = 0.15
-    subsample = 0.5
+# Model Hyperparameters
+n_estimators = 200
+max_depth = 4
+learning_rate = 0.15
+subsample = 0.5
 
 # Reading Data
-if nJets == 2:
-    dfEven = pd.read_csv('../dataset-and-plotting/CSV/VHbb_data_2jet_even.csv')
-    dfOdd = pd.read_csv('../dataset-and-plotting/CSV/VHbb_data_2jet_odd.csv')
+dfEven = pd.read_csv('/training_dataset.csv') # ENTER DATASET 1 HERE
+dfOdd = pd.read_csv('/testing_dataset.csv') # ENTER DATASET 2 HERE
 
-else:
-    dfEven = pd.read_csv('../dataset-and-plotting/CSV/VHbb_data_3jet_even.csv')
-    dfOdd = pd.read_csv('../dataset-and-plotting/CSV/VHbb_data_3jet_odd.csv')
-
-
+# Initialise XGBoost Even and Odd Classifier
 xgbEven = XGBClassifier(n_estimators=n_estimators,max_depth=max_depth,learning_rate=learning_rate,subsample=subsample)
 xgbOdd = XGBClassifier(n_estimators=n_estimators,max_depth=max_depth,learning_rate=learning_rate,subsample=subsample)
 
-
-
-print("Training the " + str(nJets) + " Jet Dataset")
-xgbEven.fit(dfEven[variables], dfEven['Class'], sample_weight=dfEven['training_weight'])
-
-xgbOdd.fit(dfOdd[variables], dfOdd['Class'], sample_weight=dfOdd['training_weight'])
+# Train XGBoost Even and Odd Classifier
+xgbEven.fit(dfEven[variables], dfEven['Class'])
+xgbOdd.fit(dfOdd[variables], dfOdd['Class'])
 
 # Calculate Score of Trained BDT
 scoresEven = xgbOdd.predict_proba(dfEven[variables])[:,1]
 scoresOdd = xgbEven.predict_proba(dfOdd[variables])[:,1]
 
+# Determine Predicted Decision Value of Trained BDT
 dfEven['decision_value'] = ((scoresEven-0.5)*2)
 dfOdd['decision_value'] = ((scoresOdd-0.5)*2)
+
+# Combine Dataset of Odd and Even Model
 df = pd.concat([dfEven,dfOdd])
-figureName = "XGBoost_" + str(nJets) + "Jets_" + str(n_estimators) + "estimators_" + str(max_depth) + "depth_" + str(learning_rate) + "learnrate.pdf"
 
-h1, ax = final_decision_plot(df, figureName)
+# Save Predicted & Training Data to CSV File
+df.to_csv("XGBoost_Predicted_Dataset.csv")
 
-sensitivity2Jet = calc_sensitivity_with_error(df)
-print(str(nJets) + " Jet using the Standard BDT: "+ str(sensitivity2Jet[0]) + " ± "+ str(sensitivity2Jet[1]))
 
 print("Time Taken", time.time() - start)
 print("FINISHED")
