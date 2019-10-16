eb3d0652ffa63c8e7dfd289b4c9b2f3f7472c5b4
Louis Heery <zcaplhe@ucl.ac.uk>
2019-10-16 16:54:37 +0100

    adding sarimax & lstm models

---
+++ lstm-nn-ml/lstm_graph_plotter.py
@@ -0,0 +1,106 @@
+##################################################################
+#                                                                #
+#      LSTM Graph Plotter (lstm_graph_plotter.py)                #
+#                                                                #
+#      For plotting Graph of results obtained from the           #
+#      lstm_predictor_model.py script.                           #
+#                                                                #
+#      By Louis Heery                                            #
+#                                                                #
+##################################################################
+
+def findAnomalies((df,movingAverageRange):
+    import numpy as np
+    import pandas as pd
+
+    df.replace([np.inf, - np.inf], np.NaN, inplace = True)
+    df.fillna(0,inplace = True)
+    df['error'] = df['actual'] - df['predicted']
+    df['stdev'] = df['error'].rolling(window = movingAverageRange).std()
+    df['mean'] = df['error'].rolling(window = movingAverageRange).mean()
+    df['lowerbound'] = df['mean'] - (2 * df['stdev'])
+    df['upperbound'] = df['mean'] + (2 * df['stdev'])
+    df['anomaly_values'] = np.where(df['error'] < df['lowerbound'], df['error'], np.where(df['error'] > df['upperbound'], df['error'], np.nan))
+    df = df.sort_values(by = 'time', ascending = False)
+    return df
+
+def round_up(n, decimals = 0):
+    multiplier = 20 ** decimals
+    return math.ceil(n * multiplier) / multiplier
+
+def plotLSTMGraph(usernumber, timeData, trainData, testData, predictedData):
+    import math
+    import os
+    import warnings
+    from math import sqrt
+
+    import matplotlib.pyplot as plt
+    import matplotlib.gridspec as gridspec
+    import numpy as np
+    import pandas as pd
+    import statsmodels.api as sm
+    import statsmodels.tsa.api as smt
+    from keras.layers import LSTM, Dense
+    from keras.models import Sequential
+    from matplotlib import pyplot as plt
+    plt.switch_backend('agg')
+    from pandas import DataFrame, Series, concat, datetime, read_csv
+    from sklearn.metrics import mean_squared_error
+    from sklearn.preprocessing import MinMaxScaler
+
+    # dataframe to store predicted&testing values for detecting anomaly values
+    predicted_df = pd.DataFrame()
+    predicted_df['time'] = timeData
+    predicted_df['actual'] = testData
+    predicted_df['predicted'] = predictedData
+    predicted_df.reset_index(inplace = True)
+    del predicted_df['index']
+    predicted_df.head()
+    anomaly_df = findAnomalies((predicted_df,7)
+    anomaly_df.reset_index(inplace = True)
+    del anomaly_df['index']
+    anomaly_df.head()
+
+    # set up subplot grid
+    fig=plt.figure()
+    gridspec.GridSpec(15,10)
+    ax1 = plt.subplot2grid((17,10), (0,0), colspan = 10, rowspan = 8)
+    ax2 = plt.subplot2grid((17,10), (9,0), colspan = 10, rowspan = 6)
+
+    ##### Subplot 1 #####
+    ax1.set_title("User " + str(usernumber) + " - LSTM NN Predictor")
+    ax1.plot(timeData/24, trainData, label = 'Training Data')  # scale time scale by 24, i.e. from units of Hours to units of Days
+    ax1.plot(timeData/24, testData, label = 'Test Data')
+    ax1.plot(timeData/24, predictedData, color = "red",label = 'Predicted Data\n(of the Test Data)')
+    ax1.legend(loc = 'upper right')
+    ax1.set_xlim(0,14)
+    ax1.set_xticks(np.arange(0, 15, 1.0))
+    ax1.set_ylabel("Host User Account Activity\n (Frequency per Bin)")
+    ax1.grid()
+
+    ##### Subplot 2 #####
+    ax2.plot(timeData/24, testData - predictedData, label = 'Residual (Test - Predicted)', color = 'green')
+    ax2.plot(anomaly_df['time']/24, anomaly_df['anomaly_values'], 'o', label = 'Anomaly', color = 'red')
+    ax2.fill_between(anomaly_df['time']/24, anomaly_df['lowerbound'], anomaly_df['upperbound'], alpha = 0.1, label = "3 STDEV Confidence Interval")
+    ax2.set_xlabel("Time (Day)")
+    ax2.set_xlim(0,14)
+    ax2.legend(loc = 'best')
+    ax2.set_xticks(np.arange(0, 15, 1.0))
+    ax2.set_ylabel("Residual Activity\n (Frequency per Bin)")
+    ax2.grid(True)
+
+    ##### Combined Subplot setup #####
+    h,l=ax2.get_legend_handles_labels() # get labels and handles from ax2
+    ax2.get_shared_x_axes().join(ax2, ax1)
+    ax1.set_xticklabels([])
+    ax2.grid(True)
+    ax1.grid(True)
+    ax2.grid(which = 'minor', linestyle = '-', linewidth = '0.02', color = 'black')
+    ax1.grid(which = 'minor', linestyle = '-', linewidth = '0.02', color = 'black')
+
+    ##### Save Figure #####
+    fig = plt.gcf()
+    fig.set_size_inches(10, 6)
+    filename = "LSTM_NN_" + "_user" + str(usernumber) + ".pdf"
+    print("Graph saved as " + filename)
+    plt.savefig(filename, bbox_inches = 'tight',dpi = 300)
---
+++ sarimax-time-series-ml/sarimax_predictor_model.py
@@ -0,0 +1,118 @@
+##################################################################
+#                                                                #
+#      SARIMAX Predictor Model (sarimax_predictor_model.py)      #
+#                                                                #
+#      A SARIMAX Time Series model for predicting the value      #
+#      of future values of a univariate dataset.                 #
+#                                                                #
+#      By Louis Heery                                            #
+#                                                                #
+##################################################################
+
+import math
+import os
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import statsmodels.tsa.api as smt
+from matplotlib import pyplot as plt
+from pandas import read_csv
+from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.arima_model import ARIMA, ARIMAResults
+
+from sarimax_graph_plotter import find_anomaly_values, round_up, plotSARIMAXGraph
+
+# define seasonality to by weekly, can replace with "day" for day long seasonality time series analysis
+week_or_day_seasonality = "week"
+
+def lstmPredictorModel(usernumber):
+    print("******")
+    print("User Number = " + str(usernumber))
+    print("******")
+
+    # import user time series data -> to be used as training&testing time series
+    filename = "lanl_dataset_user" + str(usernumber) + ".csv"
+    user = np.genfromtxt(filename, delimiter=",")
+    df = pd.DataFrame(user)
+
+    # only use time series events of type '4634', i.e. "4634 - An account was logged on" on https://csr.lanl.gov/data/2017.html
+    df_login = df[[0,1]].loc[df[1] == 4634]
+    df_login.head()
+
+    # bin dataset into hour long intervals
+    minimum, maximum = df_login[0].min(), df_login[0].max()
+    intervalLength = 60 * 60 # can be changed to specify interval length (units: seconds)
+    intervalBins = np.linspace(minimum, maximum, (maximum - minimum)/intervalLength)
+    binned_temp = pd.cut(df_login[0], intervalBins)
+    binnedValues = binned_temp.value_counts(sort=False).as_matrix()
+
+    time = np.delete(bins, -1)
+    binnedDataset = np.vstack((time,binnedValues))
+    binned_df = (pd.DataFrame({"time": binnedDataset[0], "actual": binnedDataset[1]}))
+    binned_df = binned_df.replace(0.0, 0.00000000001) # replace any 0.0 frequency bins in dataset with 0.00000000001 as 0.0 is not compatiable with log10 distortion
+    actualValues = binned_df.actual.values
+
+    # define seasonality duration of time series of dataset as 1 day or 1 week; and size of training&testing dataset
+    if week_or_day_seasonality == "day":
+        number_of_days = 7
+        number_of_datapoints = number_of_days * 24
+        train, test = actualValues[0:number_of_datapoints], actualValues[number_of_datapoints:number_of_datapoints*2]
+        trainLog, testLog = np.log10(train), np.log10(test)
+        modelOrder = (1, 1, 1)
+        modelSeasonalOrder = (0, 1, 1, 24)
+
+    if week_or_day_seasonality == "week":
+        number_of_weeks = 2
+        number_of_datapoints = number_of_weeks * 7 * 24
+        train, test = actualValues[0:number_of_datapoints], actualValues[number_of_datapoints:number_of_datapoints*2]
+        trainLog, testLog = np.log10(train), np.log10(test)
+
+        # SARIMAX hyperparameters
+        modelSeasonalOrder = (1, 0, 0, 24*7)
+        # modelOrder = (1, 1, 1) # SLOWER (2X slower), better convergence
+        modelOrder = (1, 1, 0) # COMPREMISE
+        # modelOrder = (0, 0, 0) # FASTER, poorer convergence
+
+    # initiate array to store predicted dataset
+    historicDataset = [x for x in trainLog]
+    predictionsArray = list()
+
+    # iterate SARIMAX prediction model through entire testing data time series range (0, len(testLog))
+    t = 0
+    while t < len(testLog):
+        if t % 10 == 0 or t == 1:
+            print("Testing on " + str(t) + "/" + str(testLog - 1))
+        model = sm.tsa.SARIMAX(historicDataset, order = modelOrder, seasonal_order = modelSeasonalOrder,enforce_stationarity = False,enforce_invertibility = False)
+        modelFit = model.fit(disp=0)
+        output = modelFit.forecast()
+        predictedValue = 10**output[0]
+        predictionsArray.append(predictedValue)
+        newPrediction = testLog[t]
+        historicDataset.append(newPrediction)
+        t = t + 1
+
+    # save model as .pkl file
+    exec("modelFit.save('user" + str(usernumber) + "_model.pkl')")
+
+    # save data to individual arrays
+    timeData = np.asarray(np.linspace(0, number_of_datapoints - 1, number_of_datapoints))
+    trainData = np.asarray(dataset[1, 0:number_of_datapoints])
+    testData = np.asarray(dataset[1, number_of_datapoints:number_of_datapoints*2])
+    predictedData = np.asarray(predictionsArray)
+
+    # save train, test and predicted data to CSV file
+    datasaver = np.vstack((timeData,trainData,testData,predictedData))
+    filename = "sarimax_results_user" + str(usernumber) + ".csv"
+    np.savetxt(filename, datasaver, delimiter=',')
+
+    # plot graph & save graph as PNG
+    plotSARIMAXGraph(usernumber, timeData, trainData, testData, predictedData)
+
+# iterate code through array of user numbers
+for usernumber in (1,2,2255,6763,7449,8248,8710,19779):
+    lstmPredictorModel(usernumber)
+
+print("DONE")
